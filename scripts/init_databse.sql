71f7b9a248ffd254fd84b6c9dc2151f21ea9ce61
blsia <manusami6765@gmail.com>
2025-07-14 16:08:55 +0700

    Update init_databse.sql

--- scripts/init_databse.sql
+++ scripts/init_databse.sql
@@ -1,1 +1,21 @@
+-- This file is for creating a new database named "DataWarehouse"
 
+
+USE master;
+GO
+
+END;
+GO
+
+ CREATE DATABASE DataWarehouse;
+ GO
+
+ USE DataWarehouse;
+ GO
+
+ CREATE SCHEMA bronze;
+ GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
